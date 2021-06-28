3d7166ba7e597c92ce2b72ad52a905aa5d1dce4b
Vinícius Amirat <viniciusamirat39@gmail.com>
2021-06-28 02:52:15 -0300

    Update no menu

--- contatos/addContato.php
+++ contatos/addContato.php
@@ -38,11 +38,8 @@ config();
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <div class="collapse navbar-collapse" id="collapsibleNavbar" style="text-align: right;">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="../perfil_usu/">Meu perfil</a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="../inc/exit.php">Sair</a>
                 </li>
